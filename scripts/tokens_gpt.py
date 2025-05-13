7e5057507f711f01ec4293f6c281787519779fad
Luisa <luisastellet@id.uff.br>
2025-05-13 15:38:32 -0300

    rodando as métricas e tokens

--- scripts/tokens_gpt.py
+++ scripts/tokens_gpt.py
@@ -1,7 +1,7 @@
 import json
 import tiktoken
 
-with open('dataset_newsmet/gpt/prompt1/frases_traduzidas.json', 'r', encoding='utf-8') as file:
+with open('dataset_newsmet/gpt/prompt2/frases_traduzidas.json', 'r', encoding='utf-8') as file:
     dados = json.load(file)
 
 vetor = []
@@ -25,6 +25,6 @@ for objeto in dados:
     print(f"Frase {i}")
     i += 1
 
-with open('dataset_newsmet/gpt/prompt1/frases_traduzidas.json', 'w', encoding='utf-8') as file:
+with open('dataset_newsmet/gpt/prompt2/frases_traduzidas.json', 'w', encoding='utf-8') as file:
     json.dump(vetor, file, ensure_ascii=False, indent=4)
 
