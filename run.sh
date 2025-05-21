b02b2ea57dbf7a5ea62e2113538c60bd22729b69
Luisa <luisastellet@id.uff.br>
2025-05-21 18:03:16 -0300

    iniciando a rodar o alma

--- run.sh
+++ run.sh
@@ -5,14 +5,18 @@ HF_TOKEN="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"
 INPUT_PATH="logs/"
 
 
-MODEL_ID="meta-llama/Llama-3.1-8B-Instruct"
-OUTPUT_PATH="dataset_newsmet/llama/"
-CUDA_VISIBLE_DEVICES=0 python3 generate/genPTtoEN_locais.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/llama/prompt2/llama-log.txt" 2>&1 
+# MODEL_ID="meta-llama/Llama-3.1-8B-Instruct"
+# OUTPUT_PATH="dataset_newsmet/llama/"
+# CUDA_VISIBLE_DEVICES=0 python3 generate/genPTtoEN_locais.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/llama/prompt2/llama-log.txt" 2>&1 
 
-MODEL_ID="Helsinki-NLP/opus-mt-ROMANCE-en"
-OUTPUT_PATH="dataset_newsmet/marian/"
-CUDA_VISIBLE_DEVICES=0 python3 generate/tradPTtoEN.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/marian/marian-log.txt" 2>&1 
+# MODEL_ID="Helsinki-NLP/opus-mt-ROMANCE-en"
+# OUTPUT_PATH="dataset_newsmet/marian/"
+# CUDA_VISIBLE_DEVICES=0 python3 generate/tradPTtoEN.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/marian/marian-log.txt" 2>&1 
 
-MODEL_ID="facebook/nllb-200-3.3B"
-OUTPUT_PATH="dataset_newsmet/meta/"
-CUDA_VISIBLE_DEVICES=0 python3 generate/tradPTtoEN.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/meta/nllb-log.txt" 2>&1 
+# MODEL_ID="facebook/nllb-200-3.3B"
+# OUTPUT_PATH="dataset_newsmet/meta/"
+# CUDA_VISIBLE_DEVICES=0 python3 generate/tradPTtoEN.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/meta/nllb-log.txt" 2>&1 
+
+MODEL_ID="haoranxu/ALMA-13B"
+OUTPUT_PATH="dataset_newsmet/alma/"
+CUDA_VISIBLE_DEVICES=0 python3 generate/genENtoPT_locais.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/alma/prompt1/alma-log.txt" 2>&1 
