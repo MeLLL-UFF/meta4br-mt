3d02b579ee635fb08d8141be6fb41fda7701e8bb
Luisa <luisastellet@id.uff.br>
2025-05-21 22:17:29 -0300

    tentando usar o alma, mas ta indo lixo pro json

--- run1.sh
+++ run1.sh
@@ -4,15 +4,11 @@ HF_TOKEN="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"
 INPUT_PATH="logs/"
 
 
-# MODEL_ID="mistralai/Ministral-8B-Instruct-2410"
-# OUTPUT_PATH="dataset_newsmet/mistral/"
-# CUDA_VISIBLE_DEVICES=1 python3 generate/genPTtoEN_locais.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/mistral/prompt2/mistral-log.txt" 2>&1 
+MODEL_ID="mistralai/Ministral-8B-Instruct-2410"
+OUTPUT_PATH="dataset_newsmet/mistral/"
+CUDA_VISIBLE_DEVICES=1 python3 generate/genPTtoEN_locais.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/mistral/prompt2/mistral-log.txt" 2>&1 
 
-# MODEL_ID="Qwen/Qwen2.5-7B-Instruct"
-# OUTPUT_PATH="dataset_newsmet/qwen/"
-# CUDA_VISIBLE_DEVICES=1 python3 generate/genPTtoEN_locais.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/qwen/prompt2/qwen-log.txt" 2>&1 
-
-MODEL_ID="haoranxu/ALMA-13B"
-OUTPUT_PATH="dataset_newsmet/alma/"
-CUDA_VISIBLE_DEVICES=1 python3 generate/genENtoPT_locais.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/alma/prompt2/alma-log.txt" 2>&1 
+MODEL_ID="Qwen/Qwen2.5-7B-Instruct"
+OUTPUT_PATH="dataset_newsmet/qwen/"
+CUDA_VISIBLE_DEVICES=1 python3 generate/genPTtoEN_locais.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/qwen/prompt2/qwen-log.txt" 2>&1 
 
