c0949ceafc03c833ea2ad84b9d03e528bd5c0dc5
Luisa <luisastellet@id.uff.br>
2025-05-25 19:33:48 -0300

    Terminei de rodar todas as métricas do comet para todos os modelos e datasets

--- run.sh
+++ run.sh
@@ -9,11 +9,14 @@ INPUT_PATH="logs/"
 # OUTPUT_PATH="dataset_newsmet/llama/"
 # CUDA_VISIBLE_DEVICES=0 python3 generate/genPTtoEN_locais.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/llama/prompt2/llama-log.txt" 2>&1 
 
-MODEL_ID="Helsinki-NLP/opus-mt-ROMANCE-en"
-OUTPUT_PATH="dataset_newsmet/marian/"
-CUDA_VISIBLE_DEVICES=0 python3 generate/tradPTtoEN.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/marian/marian-log.txt" 2>&1 
+# MODEL_ID="Helsinki-NLP/opus-mt-ROMANCE-en"
+# OUTPUT_PATH="dataset_newsmet/marian/"
+# CUDA_VISIBLE_DEVICES=0 python3 generate/tradPTtoEN.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/marian/marian-log.txt" 2>&1 
 
-MODEL_ID="facebook/nllb-200-3.3B"
-OUTPUT_PATH="dataset_newsmet/meta/"
-CUDA_VISIBLE_DEVICES=0 python3 generate/tradPTtoEN.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/meta/nllb-log.txt" 2>&1 
+# MODEL_ID="facebook/nllb-200-3.3B"
+# OUTPUT_PATH="dataset_newsmet/meta/"
+# CUDA_VISIBLE_DEVICES=0 python3 generate/tradPTtoEN.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/meta/nllb-log.txt" 2>&1 
 
+MODEL_ID="google/gemma-3-12b-it"
+OUTPUT_PATH="dataset_newsmet/gemma3/"
+python3 generate/genPTtoEN_locais.py --model_id $MODEL_ID --hf_token $HF_TOKEN --output_path $OUTPUT_PATH > "dataset_newsmet/gemma3/prompt2/gemma3-log.txt" 2>&1 
--- scripts/rodar_comet.py
+++ scripts/rodar_comet.py
@@ -1,41 +1,54 @@
 import json
 from comet import download_model, load_from_checkpoint
+from huggingface_hub import login
 import torch
 
+login(token="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")
+
 # model_path = download_model("Unbabel/wmt22-comet-da")
-kiwi_model_path = download_model("Unbabel/wmt23-cometkiwi-da-xxl")
-# xcomet_model_path = download_model("Unbabel/XCOMET-XXL")
+kiwi_model_path = download_model("Unbabel/wmt23-cometkiwi-da-xl")
+xcomet_model_path = download_model("Unbabel/XCOMET-XL")
 
 # model = load_from_checkpoint(model_path)
 model_kiwi = load_from_checkpoint(kiwi_model_path)
-# model_xcomet = load_from_checkpoint(xcomet_model_path)
+model_xcomet = load_from_checkpoint(xcomet_model_path)
 
-with open('dataset_manualdata/gemini/prompt1/frases_traduzidas_com_metricas.json', 'r', encoding='utf-8') as file:
-    dados = json.load(file)
+# dataset = ["dataset_newsmet/", "dataset_manueladata/"]
+# pasta1 =["gemma3/prompt1/", "gemma3/prompt2/", "gpt/prompt1/", "gpt/prompt2/", "llama/promp1/", "llama/prompt2/", "marian/", "meta/", "mistral/prompt1/", "mistral/prompt2/", "qwen/prompt1/", "qwen/prompt2/"]
+datasets = ["dataset_newsmet/"]
+pastas =["gemini/prompt1/", "gemini/prompt2/"]
 
-comet_data = [
-    {
-        "src": obj["portugues_traduzido"],
-        "mt": obj["ingles_traduzido"],
-        "ref": obj["ingles_original"],
-    }
-    for obj in dados
-]
 
-# results = model.predict(comet_data)
-# torch.cuda.empty_cache()
 
-results_kiwi = model_kiwi.predict(comet_data, batch_size=8)
-torch.cuda.empty_cache()
+for dataset in datasets:
+    for pasta in pastas:
+        # with open(f'{pasta}{pasta2}frases_traduzidas.json', 'r', encoding='utf-8') as file:
+        with open(f'{dataset}{pasta}frases_traduzidas_com_metricas.json', 'r', encoding='utf-8') as file:
+            dados = json.load(file)
 
-# results_xcomet = model_xcomet.predict(comet_data, batch_size=1)
-# torch.cuda.empty_cache()
+        comet_data = [
+            {
+                "src": obj["portugues_traduzido"],
+                "mt": obj["ingles_traduzido"],
+                "ref": obj["ingles_original"],
+            }
+            for obj in dados
+        ]
 
-for i in range(len(dados)):
-    # dados[i]["COMET22"] = {"scores": results.scores[i]}
-    dados[i]["KIWI-XXL"] = {"scores": results_kiwi.scores[i]}
-    # dados[i]["XCOMET"] = {"scores": results_xcomet.scores[i]}
-    print(f"Frase {i}.")
+        # results = model.predict(comet_data)
+        # torch.cuda.empty_cache()
 
-with open('dataset_manualdata/gemini/prompt2/frases_traduzidas_com_metricas.json', 'w', encoding='utf-8') as file:
-    json.dump(dados, file, ensure_ascii=False, indent=4)
+        results_kiwi = model_kiwi.predict(comet_data)
+        torch.cuda.empty_cache()
+
+        results_xcomet = model_xcomet.predict(comet_data)
+        torch.cuda.empty_cache()
+
+        for i in range(len(dados)):
+            # dados[i]["COMET22"] = {"scores": results.scores[i]}
+            dados[i]["KIWI-XL"] = {"scores": results_kiwi.scores[i]}
+            dados[i]["XCOMET-XL"] = {"scores": results_xcomet.scores[i]}
+            print(f"Frase {i}. {dataset}{pasta}")
+
+        with open(f'{dataset}{pasta}frases_traduzidas_com_metricas.json', 'w', encoding='utf-8') as file:
+            json.dump(dados, file, ensure_ascii=False, indent=4)
