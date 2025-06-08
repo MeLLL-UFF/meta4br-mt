a57bc95e3e48b6aaebb50252505554fe20118a59
Luisa <luisastellet@id.uff.br>
2025-06-08 17:55:34 -0300

    ajeitei o gemini prompt1 q as metricas estavam erradas

--- scripts/rodar_comet.py
+++ scripts/rodar_comet.py
@@ -15,8 +15,8 @@ model_xcomet = load_from_checkpoint(xcomet_model_path)
 
 # datasets = ["dataset_newsmet/", "dataset_manueladata/"]
 # pastas =["gemma3/prompt1/", "gemma3/prompt2/", "gpt/prompt1/", "gpt/prompt2/", "llama/promp1/", "llama/prompt2/", "marian/", "meta/", "mistral/prompt1/", "mistral/prompt2/", "qwen/prompt1/", "qwen/prompt2/"]
-datasets = ["dataset_newsmet/"]
-pastas = ["gemma3/prompt1/"]
+datasets = ["dataset_manualdata/"]
+pastas = ["gemini/prompt2/"]
 
 for dataset in datasets:
     for pasta in pastas:
