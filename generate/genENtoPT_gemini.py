c9d55e6d1ec8b7d9eaac4b6f16149a9dd0a00c0f
Luisa Stellet <luisa.stellet@id.uff.br>
2025-06-08 17:48:36 -0300

    gerando medias

--- generate/genENtoPT_gemini.py
+++ generate/genENtoPT_gemini.py
@@ -6,7 +6,7 @@ from google.genai import types
 
 #pip3 install -U -q "google-genai"
 
-df = pd.read_csv("comparacao_datasets/newsmet.csv", encoding='utf-8')
+df = pd.read_parquet("comparacao_datasets/manual_data.parquet")
 
 client = genai.Client(
     vertexai=True,
@@ -18,7 +18,7 @@ delay = 15
 
 anotacoes = []
 
-for frase in df['Text'][990:993]:
+for frase in df['Sentence']:
 
     prompt1 = f"Traduzir a frase '{frase}' do inglês para o português. Apenas escreva a frase traduzida, nada além disso"
     prompt2 = f"Traduzir a frase '{frase}' do inglês para o português. Apenas escreva a frase traduzida, nada além disso. A frase pode ou não conter metáfora"
@@ -26,7 +26,7 @@ for frase in df['Text'][990:993]:
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-lite",
-        contents=prompt2,
+        contents=prompt1,
     )
 
     
@@ -38,7 +38,7 @@ for frase in df['Text'][990:993]:
     anotacoes.append(result)
 
     # Isso aqui acaba reescrevendo o json mil vezes, mas é bom pq se der problema na máquina, não perco todas as frases, consigo continuar de onde parei
-    with open('dataset_newsmet/gemini/prompt2/ENtoPT2.json', 'w', encoding='utf-8') as f:
+    with open('dataset_manualdata/gemini/prompt1/ENtoPTnovo1.json', 'w', encoding='utf-8') as f:
         json.dump(anotacoes, f, ensure_ascii=False, indent=5)
 
     time.sleep(delay)
