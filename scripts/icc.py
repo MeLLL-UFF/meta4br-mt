722c6c461f01e29b5c7474601bb3402a4fef9496
Luisa Stellet <luisa.stellet@id.uff.br>
2025-06-09 09:57:22 -0300

    Finalizei os icc

--- scripts/icc.py
+++ scripts/icc.py
@@ -1,12 +1,13 @@
 import pingouin as pg
 import pandas as pd
 import json
+import os
 
 # Um pra prompt1 e tradicionais e um pra prompt2 e tradicionais
 # ICC1
 # Fazer um geral pra todos os modelos, seja prompt1, prompt2 e tradicionais
 
-def montar_df(modelos, dataset, df, object, metricas, frases):
+def montar_df(modelos, dataset, object, metricas, frases):
     dados_modelos = {}
     
     # Lê todos os arquivos de uma vez e armazena em memória
@@ -29,62 +30,78 @@ def montar_df(modelos, dataset, df, object, metricas, frases):
                 object["score"].append(dados[i][nome_metrica][chave])
     
     df = pd.DataFrame(object)
-    print(df.head())
     return df
         
+def gerar_icc(dataset, complemento, metrica):
+    path = f"dataset_{dataset}/[CSV] calculo_icc/df_{complemento}.csv"
+    if not os.path.exists(path):
+        print(f"Arquivo {path} não encontrado. Pulando ICC para {complemento} - {metrica}.")
+    df = pd.read_csv(path)
+
+    df_metrica_atual = df[df["metrica"] == metrica]
+    print(df_metrica_atual.head())
+
+    icc = pg.intraclass_corr(data=df_metrica_atual, targets='frases_ingles', raters='modelo', ratings='score').round(3)
+    print(icc)
+    icc.to_csv(f"dataset_{dataset}/[CSV] calculo_icc/icc_{complemento}/icc_{metrica}_{complemento}.csv", index=False)
+
+
 
 if __name__ == "__main__":
     datasets = ["manualdata", "newsmet"]
-    modelos_prompt1 = ["gemini/prompt1/", "gemma3/prompt1/", "gemmaX", "gpt/prompt1/", "llama/prompt1/", "marian", "meta", "mistral/prompt1/", "qwen/prompt1/"]
-    modelos_prompt2 = ["gemini/prompt2/", "gemma3/prompt2/", "gemmaX", "gpt/prompt2/", "llama/prompt2/", "marian", "meta", "mistral/prompt2/","qwen/prompt2/"]
-    todos_modelos = ["gemini/prompt1/", "gemma3/prompt1/", "gpt/prompt1/", "llama/prompt1/", "mistral/prompt1/", "qwen/prompt1/", "gemini/prompt2/", "gemma3/prompt2/", "gpt/prompt2/", "llama/prompt2/", "mistral/prompt2/","qwen/prompt2/", "gemmaX", "marian", "meta"]
+    modelos = [
+        ["gemini/prompt1/", "gemma3/prompt1/", "gemmaX", "gpt/prompt1/", "llama/prompt1/", "marian", "meta", "mistral/prompt1/", "qwen/prompt1/"],
+        ["gemini/prompt2/", "gemma3/prompt2/", "gemmaX", "gpt/prompt2/", "llama/prompt2/", "marian", "meta", "mistral/prompt2/","qwen/prompt2/"]
+    ]
+    modelos_gerais = ["gemini/prompt1/", "gemma3/prompt1/", "gpt/prompt1/", "llama/prompt1/", "mistral/prompt1/", "qwen/prompt1/", "gemini/prompt2/", "gemma3/prompt2/",  "gpt/prompt2/", "llama/prompt2/",  "mistral/prompt2/", "qwen/prompt2/","gemmaX", "marian", "meta",]
+
     vet_manualdata = pd.read_parquet("comparacao_datasets/manual_data.parquet")["Sentence"].tolist()
     vet_newsmet = pd.read_csv("comparacao_datasets/newsmet.csv")["Text"].tolist()
 
     metricas = ["ROUGE/rougeL", "BLEU/bleu", "BERTSCORE/f1", "BLEURT/scores", "COMET22/scores", "KIWI-XL/scores", "XCOMET-XL/scores"]
     for dataset in datasets:
-        object = {
-            "frases_ingles": [],
-            "metrica": [],
-            "modelo": [],
-            "score": [],
-        }
 
         df = pd.DataFrame()
         if dataset == "manualdata":
-            df = montar_df(modelos_prompt1, dataset, df, object, metricas, vet_manualdata)
-            df.to_csv(f"dataset_{dataset}/df_prompt1.csv", index=False)
-            df = montar_df(modelos_prompt2, dataset, df, object, metricas, vet_manualdata)
-            df.to_csv(f"dataset_{dataset}/df_prompt2.csv", index=False)
-            df = montar_df(todos_modelos, dataset, df, object, metricas, vet_manualdata)
-            df.to_csv(f"dataset_{dataset}/df_geral.csv", index=False)
+            for modelo in modelos:
+                object = {
+                    "frases_ingles": [],
+                    "metrica": [],
+                    "modelo": [],
+                    "score": [],
+                }
+                df = montar_df(modelo, dataset, object, metricas, vet_manualdata)
+                df.to_csv(f"dataset_{dataset}/[CSV] calculo_icc/df_{modelo[0].split("/")[1]}.csv", index=False)
+                
+            object = {
+                "frases_ingles": [],
+                "metrica": [],
+                "modelo": [],
+                "score": [],
+            }
+            df = montar_df(modelos_gerais, dataset, object, metricas, vet_manualdata)
+            df.to_csv(f"dataset_{dataset}/[CSV] calculo_icc/df_geral.csv", index=False)
         elif dataset == "newsmet":
-            df = montar_df(modelos_prompt1, dataset, df, object, metricas, vet_newsmet)
-            df.to_csv(f"dataset_{dataset}/df_prompt1.csv", index=False)
-            df = montar_df(modelos_prompt2, dataset, df, object, metricas, vet_newsmet)
-            df.to_csv(f"dataset_{dataset}/df_prompt2.csv", index=False)
-            df = montar_df(todos_modelos, dataset, df, object, metricas, vet_newsmet)
-            df.to_csv(f"dataset_{dataset}/df_geral.csv", index=False)
+            for modelo in modelos:
+                object = {
+                    "frases_ingles": [],
+                    "metrica": [],
+                    "modelo": [],
+                    "score": [],
+                }
+                df = montar_df(modelo, dataset, object, metricas, vet_newsmet)
+                df.to_csv(f"dataset_{dataset}/[CSV] calculo_icc/df_{modelo[0].split('/')[1]}.csv", index=False)
+            
+            object = {
+                "frases_ingles": [],
+                "metrica": [],
+                "modelo": [],
+                "score": [],
+            }
+            df = montar_df(modelos_gerais, dataset, object, metricas, vet_newsmet)
+            df.to_csv(f"dataset_{dataset}/[CSV] calculo_icc/df_geral.csv", index=False)
 
-    df_manualdata1 = pd.read_csv("dataset_manualdata/df_prompt1.csv")
-    df_manualdata2 = pd.read_csv("dataset_manualdata/df_prompt2.csv")
-    df_manualdata_geral = pd.read_csv("dataset_manualdata/df_geral.csv")
-
-    df_newsmet1 = pd.read_csv("dataset_newsmet/df_prompt1.csv")
-    df_newsmet2 = pd.read_csv("dataset_newsmet/df_prompt2.csv")
-    df_newsmet_geral = pd.read_csv("dataset_newsmet/df_geral.csv")
-
-
-    icc_manualdata1 = pg.intraclass_corr(data=df_manualdata1, targets='frases_ingles', raters='modelo', ratings='score').round(3)
-    icc_manualdata1.to_csv("dataset_manualdata/icc_final_prompt1.csv", index=False)
-    icc_manualdata2 = pg.intraclass_corr(data=df_manualdata2, targets='frases_ingles', raters='modelo', ratings='score').round(3)
-    icc_manualdata2.to_csv("dataset_manualdata/icc_final_prompt2.csv", index=False)
-    icc_manualdata_geral = pg.intraclass_corr(data=df_manualdata_geral, targets='frases_ingles', raters='modelo', ratings='score').round(3)
-    icc_manualdata_geral.to_csv("dataset_manualdata/icc_final_geral.csv", index=False)
-
-    icc_newsmet1 = pg.intraclass_corr(data=df_newsmet1, targets='frases_ingles', raters='modelo', ratings='score').round(3)
-    icc_newsmet1.to_csv("dataset_newsmet/icc_final_prompt1.csv", index=False)
-    icc_newsmet2 = pg.intraclass_corr(data=df_newsmet2, targets='frases_ingles', raters='modelo', ratings='score').round(3)
-    icc_newsmet2.to_csv("dataset_newsmet/icc_final_prompt2.csv", index=False)
-    icc_newsmet_geral = pg.intraclass_corr(data=df_newsmet_geral, targets='frases_ingles', raters='modelo', ratings='score').round(3)
-    icc_newsmet_geral.to_csv("dataset_newsmet/icc_final_geral.csv", index=False)
+        for metrica in metricas:
+            gerar_icc(dataset, "prompt1", metrica.split("/")[0])
+            gerar_icc(dataset, "prompt2", metrica.split("/")[0])
+            gerar_icc(dataset, "geral", metrica.split("/")[0])
