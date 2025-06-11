798cb64c5e6adc657c2d9bbc027d73c3beddf4bc
Luisa Stellet <luisa.stellet@id.uff.br>
2025-06-11 09:27:11 -0300

    Rodando o gemmaX pro common já que na avaliação humana ficou faltando esse modelo

--- convert_xlxs.py
+++ convert_xlxs.py
@@ -2,11 +2,11 @@ import pandas as pd
 import json
 
 # Abrir e carregar o arquivo JSON
-with open('qwen/frases_traduzidas.json', 'r', encoding='utf-8') as f:
+with open('dataset_common/gemmaX/frases_traduzidas.json', 'r', encoding='utf-8') as f:
     data = json.load(f)  # data é uma lista de dicionários
 
 # Converter para DataFrame
 df = pd.DataFrame(data)
 
 # Salvar como Excel
-df.to_excel('planilhas/qwen.xlsx', index=False)
+df.to_excel('planilhas/gemmaX.xlsx', index=False)
